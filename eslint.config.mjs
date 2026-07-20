6b63cd896de5d6f51452bb0d0ecb1c8d104eb01e
Zesmart <15648640997@163.com>
2026-07-20 22:47:34 +0800

    禁用setState-in-effect ESLint规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
     // React rules
     "react-hooks/exhaustive-deps": "off",
     "react-hooks/purity": "off",
+    "react-hooks/set-state-in-effect": "off",
     "react/no-unescaped-entities": "off",
     "react/display-name": "off",
     "react/prop-types": "off",
